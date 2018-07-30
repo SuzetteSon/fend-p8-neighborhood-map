19dc2d9e1eb203dbd683fe139da770a4219769b3
Suzette Sonnekus <suzetsonekis@gmail.com>
2018-07-30 15:00:16 +0200

    Built map component

---
+++ src/mapComp.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+import GoogleMapReact from 'google-map-react'
+
+const NameComp = ({text}) => <div>{text}</div>;
+
+class MapComp extends Component {
+	static defaultProps = {
+		center: { lat: 59.329323, lng: 18.068581},
+		zoom: 11
+	}
+
+	render() {
+		return (
+			<div className='google-map'>
+				<GoogleMapReact
+				bootstrapURLKeys={{key: 'AIzaSyB22o9GOGwBbO3u6tUacs4or8gxnFmI9jU'}}
+					defaultCenter={this.props.center}
+					defaultZoom={this.props.zoom}>
+					<NameComp
+						lat={59.329323}
+	            		lng={18.068581}
+	            		text={'Hello Stockholm'}
+	            		/>
+				</GoogleMapReact>
+			</div>
+		)
+	}
+}
+export default MapComp
